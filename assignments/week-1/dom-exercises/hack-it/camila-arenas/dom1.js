e2c18032f2eaee854c775c5cdbcec72672cb49b7
Camila Arenas <mariacamilaarenasd>
2014-11-20 16:33:37 -0800

    Add Camila dom

---
+++ assignments/week-1/dom-exercises/hack-it/camila-arenas/dom1.js
@@ -0,0 +1,4 @@
+// https://www.yahoo.com/
+
+document.querySelector("#yui_3_8_1_1_1416528453779_685").setAttribute("src","http://www.nasa.gov/images/content/706436main_20121114-304-193Blend_M6-orig_full.jpg")
+
